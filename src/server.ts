bd22163df6b4aee0c138f9f0c389e4602762d2ab
imam-hossain <zunayedemon6t9@gmail.com>
2023-06-26 20:19:02 +0600

    server setup successfully

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,26 @@
+import mongoose from 'mongoose';
 import app from './app';
+import config from './config';
 
-const 
+const {port, database_url} = config
+
+
+
+const run = async ()=>{
+    try{
+        mongoose.connect(database_url as string)
+        .then(()=> {
+            console.log("database connected successfully");
+        })
+
+        app.listen(port , ()=>{
+            console.log(`server is running on port ${port}`);
+        })
+
+    }
+    catch(error){
+        console.log(error)
+    }
+
+}
+run().catch(error => console.log(error))
