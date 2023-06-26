2fec74bc2876cdd2aa90b36dfcd48b40c01a5df7
imam-hossain <zunayedemon6t9@gmail.com>
2023-06-26 21:43:13 +0600

    create apiError successfully

--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -1,7 +1,10 @@
+import { httpStatus } from 'http-status';
 import config from "../../../config";
 import { IUser } from "./user.interface"
 import { User } from "./user.model"
 import { generateUserId } from "./user.utils";
+import ApiError from '../../../error/ApiError';
+
 
 const createUser = async (user: IUser): Promise<IUser | null> => {
     const id = await generateUserId();
@@ -13,7 +16,7 @@ const createUser = async (user: IUser): Promise<IUser | null> => {
     const createdUser = await User.create(user);
   
     if (!createdUser) {
-      throw new Error('Failed to create');
+      throw new ApiError(400,'Failed to create');
     }
     return createdUser;
   }
